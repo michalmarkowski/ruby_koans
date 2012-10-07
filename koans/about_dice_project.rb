48bb183b79f961c2fc235ba1c2883eb9673a0610
Michał Markowski <m@mdevel.pl>
2012-10-07 14:26:21 +0200

    Fixed 'The rolls should not be equal' for the same roll values

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -46,7 +46,7 @@ class AboutDiceProject < EdgeCase::Koan
     dice.roll(5)
     second_time = dice.values
 
-    assert_not_equal first_time, second_time,
+    assert_not_equal first_time.object_id, second_time.object_id,
       "Two rolls should not be equal"
 
     # THINK ABOUT IT:
